b5975ec1e00c45b83771af3e9cc8f3c22a85ddd9
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2022-10-11 11:59:18 +0200

    Commit echo

---
+++ src/Encriptar.kt
@@ -0,0 +1,34 @@
+fun main(){
+
+    var text = ""
+    var cifrado=""
+
+    println("Pon la contraseña")
+    text= readLine().toString()
+
+    cifrado= control(text)
+
+    println(cifrado)
+
+}
+
+fun control(text : String):String{
+    val abc="abcdefghijklmnñopqrstuvwxyz"
+    var cifrado = ""
+    val rot = 13
+
+    text.forEach {
+        if(it.isLetter()){
+            if(abc.indexOf(it)+rot<27){
+                cifrado += abc[abc.indexOf(it)+rot]
+            }else{
+                if (abc.indexOf(it)+rot>=27){
+                    cifrado += abc[(abc.indexOf(it)+rot)-27]
+                }
+            }
+        }
+    }
+
+    return cifrado
+
+}
